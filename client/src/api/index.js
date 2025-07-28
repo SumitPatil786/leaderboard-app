6923faec13120eae204de46916562a7a194e0e32
SumitPatil786 <145134522+SumitPatil786@users.noreply.github.com>
2025-07-28 23:27:55 +0530

    Update index.js

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: "http://localhost:5000/api",  
+    baseURL: "https://leaderboard-app-agol.onrender.com",  
 });
 
 export const fetchUsers = () => api.get("/users").then(r => r.data);
